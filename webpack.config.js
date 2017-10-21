779025ca37871da2ba7484478c776e256568be95
amybingzhao <abz3@duke.edu>
2017-10-21 15:14:53 -0400

    added horizontal timeline component

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,20 @@ module.exports = {
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
-    }]
+    }],
+    rules: [{
+            test: /\.less$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "less-loader", options: {
+                    strictMath: true,
+                    noIeCompat: true
+                }
+            }]
+        }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
